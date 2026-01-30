5eaea570b3003c2923b08dc3474b78c4e56f1471
Morgan Lindbom <din-email@example.com>
2026-01-30 12:19:42 +0100

    Replace bcrypt with bcryptjs for Termux compatibility

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const { exec } = require('child_process');
 const sqlite3 = require('sqlite3').verbose();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 
 const app = express();
